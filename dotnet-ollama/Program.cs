c1620f9175738b14229be6408188eca453e08e7b
Thiago S Adriano <tadriano.net@gmail.com>
2025-02-09 11:07:26 -0300

    first commit

---
+++ dotnet-ollama/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddChatClient(new OllamaChatClient(new Uri("http://localhost:11434"), "llama3.1"));
+
+var app = builder.Build();
+
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+var chatCompletion = await chatClient.CompleteAsync("Quem é mais rápido o Superman ou o Flash?");
+
+Console.WriteLine(chatCompletion.Message.Text);
